fa842b05f7615072ad179d8c85da4166e5ca4e37
GustavoM137 <gustauvomenezes@gmail.com>
2026-09-11 18:21:56 -0300

    Refactor quantization error calculation in q4L3.c

--- q4L3.c
+++ q4L3.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 int main() {
-    unsigned char x, e, e_medio = 0, n;
+    unsigned char x, e, n;
+    float e_medio = 0;
 
     puts("Digite a quantidade n de amostras do sinal:");
     scanf("%hhu", &n);
@@ -10,7 +11,7 @@ int main() {
         puts("Digite o valor da amostra:");
         scanf("%hhu", &x);
 
-        e = (x > (x & 0xF0)) ? x - (x & 0xF0) : (x & 0xF0) - x;
+        e = x & 0x0F; // |x - ^x| = |x - (x & 0xF0)| = |XXXX XXXX - XXXX 0000| = |0000 XXXX| = 0000 XXXX = x & 0x0F
 
         printf("Valor original: %hhu\n", x);
         printf("Valor quantizado: %hhu\n", x >> 4);
@@ -20,7 +21,7 @@ int main() {
         e_medio += e;
     }
 
-    printf("Erro médio de quantização: %.2f\n", (float)e_medio / n);
+    printf("Erro médio de quantização: %.2f\n", e_medio / n);
 
     return 0;
 }
